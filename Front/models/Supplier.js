8326783f95cb3c01b5251df903cda60680039f96
Anibalgomezfer <59883735+Anibalgomezfer@users.noreply.github.com>
2020-11-26 19:23:29 +0100

    Incluyendo supplier

--- Front/models/Supplier.js
+++ Front/models/Supplier.js
@@ -1,10 +1,2 @@
-function getSuppliers() {
-    var name = "";
-    var dni = "";
 
-    fetch('localhost:8080/api/v1/supplier')
-        .then(response => response.json())
-        .then(json => console.log(json))
-
-}
 
