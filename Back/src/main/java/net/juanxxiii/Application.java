f17d30d0616bff628bdd9a16932b750794318272
PabloViniegra <pablovpmadrid@gmail.com>
2021-01-26 01:29:34 +0100

    Probando Jasper Reports

--- Back/src/main/java/net/juanxxiii/Application.java
+++ Back/src/main/java/net/juanxxiii/Application.java
@@ -1,8 +1,13 @@
 package net.juanxxiii;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperReport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.io.InputStream;
 
 @SpringBootApplication
 @EnableJpaRepositories("net.juanxxiii.db.repository")
