fb844aa56676fb304bef914fb21f766899150fc4
PabloViniegra <pablovpmadrid@gmail.com>
2021-02-03 20:10:20 +0100

    cambios

--- Back/src/main/java/net/juanxxiii/reportService/ReportService.java
+++ Back/src/main/java/net/juanxxiii/reportService/ReportService.java
@@ -4,6 +4,10 @@ import net.juanxxiii.db.entity.Client;
 import net.juanxxiii.db.repository.ClientRepository;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
@@ -14,6 +18,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,20 +35,21 @@ public class ReportService {
     public String exportReport(String reportFormat) {
         List<Client> clients = clientRepository.findAll();
         //Load file and compile it
-
+        JasperReport jasperReport = null;
         try {
 
             InputStream stream = getClass().getResourceAsStream("/clients_template.jrxml");
-            JasperReport jasperReport = JasperCompileManager.compileReport(stream);
-
+            jasperReport = JasperCompileManager.compileReport(stream);
             JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(clients);
+
             Map<String, Object> map = new HashMap<>();
             map.put("createdBy", "Grupo 2 SGE");
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, map, dataSource);
             if (reportFormat.equals("pdf")) {
-                JasperExportManager.exportReportToPdfFile(jasperPrint,"C:\\report_client.pdf");
+                //"../Reports/report_client[" + LocalDate.now() + "].pdf"
+                JasperExportManager.exportReportToPdfFile(jasperPrint,"report_client.pdf");
             } else if (reportFormat.equals("html")) {
-                JasperExportManager.exportReportToHtmlFile(jasperPrint, "C:\\report_client.html");
+                JasperExportManager.exportReportToHtmlFile(jasperPrint, "../Reports/report_client[" + LocalDate.now() + "].html");
             }
         } catch (JRException e) {
             e.printStackTrace();
