7770d97d524e7bd1d61f2092880517786e9fcde8
Anibalgomezfer <59883735+Anibalgomezfer@users.noreply.github.com>
2021-02-10 12:41:08 +0100

    Arreglando la mierda que lio Alejandro

--- Front/dist/js/salesReportConnection.js
+++ Front/dist/js/salesReportConnection.js
@@ -22,4 +22,32 @@ async function getAllClientsInaSelected() {
                 select.appendChild(option);
             });
         })
-}
+    }
+
+    document.getElementById("generateReport").addEventListener("click", ev => {
+        ev.preventDefault();
+        generateReport();
+    })
+
+    async function generateReport(){
+
+            let client = document.getElementById("clientForReport");
+            let dateInit = document.getElementById("startDate");
+            let dateLast = document.getElementById("endDate");
+
+            let url = 'http://localhost:8080/api/v1/reports/sales/'+ client.value + '/'+ dateInit.value +'/'+ dateLast.value;
+            let getInit = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+
+            console.log(url)
+
+            await fetch(url, getInit)
+                .then(response => {
+                    console.log(response)
+                });
+    }
